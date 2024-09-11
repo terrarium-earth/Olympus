4a3ef523e0cb1934cd7f2a1146a34830369f45a5
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2024-09-11 16:35:13 -0400

    ctx

---
+++ common/src/main/java/earth/terrarium/olympus/client/components/context/ContextButtonWidget.java
@@ -0,0 +1,65 @@
+package earth.terrarium.olympus.client.components.context;
+
+import com.teamresourceful.resourcefullib.client.screens.CursorScreen;
+import earth.terrarium.olympus.client.components.base.BaseWidget;
+import earth.terrarium.olympus.client.ui.UIConstants;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+public class ContextButtonWidget extends BaseWidget {
+
+    private static final int PADDING = 5;
+    private static final int COLOR = 0xAAAAAA;
+    private static final int HOVER_COLOR = 0xFFFFFF;
+    private static final int DANGER_COLOR = 0xCA3636;
+    private static final int DANGER_HOVER_COLOR = 0xFFFFFF;
+
+    private final Component text;
+    private final Runnable action;
+    private final boolean danger;
+
+    public ContextButtonWidget(Component text, Runnable action, boolean danger) {
+        super(Minecraft.getInstance().font.width(text) + PADDING * 2, 16);
+
+        this.text = text;
+        this.action = action;
+        this.danger = danger;
+    }
+
+    @Override
+    protected void renderWidget(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+        Font font = Minecraft.getInstance().font;
+
+        if (this.isHovered) {
+            ResourceLocation texture = this.danger ? UIConstants.DANGER_BUTTON : UIConstants.BUTTON;
+            UIUtils.blitWithEdge(graphics, texture, this.getX(), this.getY(), this.getWidth(), this.getHeight(), 4);
+        }
+
+        graphics.drawString(
+            font, this.text,
+            this.getX() + PADDING, this.getY() + (this.getHeight() - 8) / 2,
+            color(this.isHovered()), false
+        );
+    }
+
+    @Override
+    public void onClick(double mouseX, double mouseY) {
+        this.action.run();
+    }
+
+    @Override
+    public CursorScreen.Cursor getCursor() {
+        return CursorScreen.Cursor.POINTER;
+    }
+
+    public int color(boolean hovered) {
+        if (this.danger) {
+            return hovered ? DANGER_HOVER_COLOR : DANGER_COLOR;
+        } else {
+            return hovered ? HOVER_COLOR : COLOR;
+        }
+    }
+}
