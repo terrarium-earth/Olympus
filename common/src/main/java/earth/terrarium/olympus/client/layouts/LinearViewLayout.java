0448d9a87b06be4d5927fd57cf9596ee0217c668
Brady <gravy@thatgravyboat.tech>
2024-09-13 14:16:44 -0230

    Layouts (#7)
    * Layouts
    
    * Gap >:

---
+++ common/src/main/java/earth/terrarium/olympus/client/layouts/LinearViewLayout.java
@@ -0,0 +1,41 @@
+package earth.terrarium.olympus.client.layouts;
+
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.layouts.LayoutElement;
+import net.minecraft.client.gui.layouts.LinearLayout;
+
+import java.util.function.Consumer;
+
+public class LinearViewLayout extends BaseLayout<LinearLayout> {
+
+    LinearViewLayout(LinearLayout.Orientation orientation) {
+        super(orientation == LinearLayout.Orientation.HORIZONTAL ? LinearLayout.horizontal() : LinearLayout.vertical());
+    }
+
+    public LinearViewLayout withGap(int gap) {
+        this.layout.spacing(gap);
+        return this;
+    }
+
+    public LinearViewLayout withChild(LayoutElement element) {
+        this.layout.addChild(element);
+        return this;
+    }
+
+    public LinearViewLayout withChildren(LayoutElement... elements) {
+        for (LayoutElement element : elements) {
+            withChild(element);
+        }
+        return this;
+    }
+
+    @Override
+    public LinearViewLayout withPosition(int x, int y) {
+        return (LinearViewLayout) super.withPosition(x, y);
+    }
+
+    @Override
+    public LinearViewLayout build(Consumer<AbstractWidget> adder) {
+        return (LinearViewLayout) super.build(adder);
+    }
+}
