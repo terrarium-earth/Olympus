d6bae35267963577c5b81d23b5fdc9441dd9c426
Adrian O.V <83074853+CodexAdrian@users.noreply.github.com>
2025-04-17 08:18:35 -0400

    Style Getting and Callbacks

--- fabric/src/example/java/earth/terrarium/example/examples/TextExample.java
+++ fabric/src/example/java/earth/terrarium/example/examples/TextExample.java
@@ -6,11 +6,19 @@ import earth.terrarium.example.base.OlympusExample;
 import earth.terrarium.olympus.client.components.Widgets;
 import earth.terrarium.olympus.client.layouts.Layouts;
 import net.minecraft.client.gui.layouts.FrameLayout;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
 
 @OlympusExample(id = "text", description = "A simple text example" )
 public class TextExample extends ExampleScreen {
     public static final String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
+    public static final Component styleTest = Component.literal(text).append(Component.literal("LINK").withStyle(Style.EMPTY.withColor(0xFFFF0000).withClickEvent(new ClickEvent(ClickEvent.Action.CHANGE_PAGE, "test")))).append(text);
+
+    public static final Logger LOGGER = LogManager.getLogger();
 
     @Override
     protected void init() {
@@ -44,13 +52,24 @@ public class TextExample extends ExampleScreen {
             widget.textAlignRight();
         }));
 
-
-        entries.withChild(Widgets.textarea(Component.literal(text), 100, widget -> {
+        entries.withChild(Widgets.textarea(styleTest, 100, widget -> {
             widget.scale(0.5f);
             widget.textAlignCenter();
+            widget.clickActionCallback(this::handleComponentClicked);
         }));
 
         entries.build(this::addRenderableWidget);
         FrameLayout.centerInRectangle(entries, 0, 0, this.width, this.height);
+    }
+
+    @Override
+    public boolean handleComponentClicked(@Nullable Style style) {
+        if (style == null || style.getClickEvent() == null) {
+            return false;
+        } else if (style.getClickEvent().getAction() == ClickEvent.Action.CHANGE_PAGE) {
+            LOGGER.info("Page changed to {}", style.getClickEvent().getValue());
+            return true;
+        }
+        return super.handleComponentClicked(style);
     }
 }
