80362fa951aa101d1a6df3f5afa55130751e7572
Mona <git@mona.cat>
2026-08-18 17:53:31 +0200

    fix: add rounded rectangle fix (#54)

--- common/src/main/java/earth/terrarium/olympus/client/pipelines/pips/RoundedTexturePIPRenderer.java
+++ common/src/main/java/earth/terrarium/olympus/client/pipelines/pips/RoundedTexturePIPRenderer.java
@@ -1,20 +1,29 @@
 package earth.terrarium.olympus.client.pipelines.pips;
 
 import com.mojang.blaze3d.PrimitiveTopology;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.textures.FilterMode;
 import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.ByteBufferBuilder;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.PoseStack;
+import earth.terrarium.olympus.client.pipelines.RoundedRectangle;
 import earth.terrarium.olympus.client.pipelines.RoundedTexture;
 import earth.terrarium.olympus.client.pipelines.renderer.PipelineRenderer;
+import earth.terrarium.olympus.client.pipelines.uniforms.RoundedRectangleUniform;
 import earth.terrarium.olympus.client.pipelines.uniforms.RoundedTextureUniform;
 import earth.terrarium.olympus.client.utils.GuiGraphicsHelper;
+import earth.terrarium.olympus.client.utils.PipRendererHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.navigation.ScreenRectangle;
 import net.minecraft.client.gui.render.TextureSetup;
 import net.minecraft.client.gui.render.pip.PictureInPictureRenderer;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.state.gui.BlitRenderState;
+import net.minecraft.client.renderer.state.gui.GuiRenderState;
+import net.minecraft.util.ARGB;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix3x2f;
 import org.joml.Vector2f;
@@ -37,6 +46,26 @@ public class RoundedTexturePIPRenderer extends PictureInPictureRenderer<RoundedT
         return this.lastState != null && this.lastState.equals(state);
     }
 
+    protected void blitTexture(State renderState, GuiRenderState guiRenderState) {
+        guiRenderState.addBlitToCurrentLayer(new BlitRenderState(
+            RenderPipelines.GUI_TEXTURED,
+            TextureSetup.singleTexture(PipRendererHelper.getTextureView(this), RenderSystem.getSamplerCache().getRepeat(FilterMode.NEAREST)),
+            renderState.pose(),
+            renderState.x0(),
+            renderState.y0(),
+            renderState.x1(),
+            renderState.y1(),
+            0.0F,
+            1.0F,
+            1.0F,
+            0.0F,
+            -1,
+            renderState.scissorArea(),
+            null)
+        );
+    }
+
+
     @Override
     protected void renderToTexture(@NotNull State state, @NonNull PoseStack poseStack, @NonNull SubmitNodeCollector submitNodeCollector) {
         var bounds = state.bounds;
@@ -53,15 +82,15 @@ public class RoundedTexturePIPRenderer extends PictureInPictureRenderer<RoundedT
             bufferBuilder.addVertex(scaledWidth, 0f, 0f).setUv(state.u1(), state.v0()).setColor(-1);
 
             PipelineRenderer.builder(RoundedTexture.PIPELINE, bufferBuilder.buildOrThrow())
-                    .uniform(RoundedTextureUniform.STORAGE, RoundedTextureUniform.of(
-                            new Vector4f(state.borderRadius()),
-                            new Vector2f(scaledWidth, scaledHeight),
-                            new Vector2f(scaledWidth / 2f, scaledHeight / 2f),
-                            scale
-                    ))
-                    .textures(state.texture())
-                    .color(state.color())
-                    .draw();
+                .uniform(RoundedTextureUniform.STORAGE, RoundedTextureUniform.of(
+                    new Vector4f(state.borderRadius()),
+                    new Vector2f(scaledWidth, scaledHeight),
+                    new Vector2f(scaledWidth / 2f, scaledHeight / 2f),
+                    scale
+                ))
+                .textures(state.texture())
+                .color(state.color())
+                .draw();
         }
 
         this.lastState = state;
@@ -73,25 +102,25 @@ public class RoundedTexturePIPRenderer extends PictureInPictureRenderer<RoundedT
     }
 
     public record State(
-            int x0, int y0, int x1, int y1,
-            float u0, float v0, float u1, float v1,
-            TextureSetup texture, int color,
-            int borderRadius,
-            Matrix3x2f pose, ScreenRectangle scissorArea, ScreenRectangle bounds
+        int x0, int y0, int x1, int y1,
+        float u0, float v0, float u1, float v1,
+        TextureSetup texture, int color,
+        int borderRadius,
+        Matrix3x2f pose, ScreenRectangle scissorArea, ScreenRectangle bounds
     ) implements OlympusPictureInPictureRenderState<State> {
 
         public State(
-                GuiGraphicsExtractor graphics,
-                int x, int y, int width, int height,
-                float u0, float v0, float u1, float v1,
-                TextureSetup texture, int color, int borderRadius
+            GuiGraphicsExtractor graphics,
+            int x, int y, int width, int height,
+            float u0, float v0, float u1, float v1,
+            TextureSetup texture, int color, int borderRadius
         ) {
             this(
-                    x, y, x + width, y + height,
-                    u0, v0, u1, v1,
-                    texture, color, borderRadius,
-                    new Matrix3x2f(graphics.pose()), GuiGraphicsHelper.getLastScissor(graphics),
-                    OlympusPictureInPictureRenderState.getRelativeBounds(graphics, x, y, x + width, y + height)
+                x, y, x + width, y + height,
+                u0, v0, u1, v1,
+                texture, color, borderRadius,
+                new Matrix3x2f(graphics.pose()), GuiGraphicsHelper.getLastScissor(graphics),
+                OlympusPictureInPictureRenderState.getRelativeBounds(graphics, x, y, x + width, y + height)
             );
         }
 
